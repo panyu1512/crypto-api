23e5732dcaff9a382ea5a2472b587dabe0be1bd0
Enrique Ferrer Agius <eferreragius.external@epo.org>
2024-02-04 17:17:58 +0100

    Add Flask routes for crypto rankings, exchange rankings, DEX rankings, NFT collections, trending cryptos, and gainers

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from scraper import Scraper
+
+app = Flask(__name__)
+scraper = Scraper()
+
+
+@app.route("/crypto-ranking", methods=["GET"])
+def get_ranking():
+    return scraper.get_top_10_crypto_by_market_cap()
+
+@app.route("/exchange-ranking", methods=["GET"])
+def get_exchanges():
+    return scraper.get_top_10_exchanges()
+
+@app.route("/dex-ranking", methods=["GET"])
+def get_dex():
+    return scraper.get_top_10_descentralized_exchanges()
+
+@app.route("/nft-collections", methods=["GET"])
+def get_nft():
+    return scraper.get_nft_top_10_collections()
+
+@app.route("/trending-cryptos", methods=["GET"])
+def get_trending():
+    return scraper.get_top_10_trending_coins()
+
+@app.route("/gainers", methods=["GET"])
+def get_gainers():
+    return scraper.get_top_10_gainers()
+
+if __name__ == "__main__":
+    app.run(debug=True)
