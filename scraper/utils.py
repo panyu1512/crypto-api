3510b1b3a7587bbbf25410ce9b7b982e2eac4ba3
Enrique Ferrer Agius <eferreragius.external@epo.org>
2024-02-04 15:15:04 +0100

    Add obtain_crypto_abbreviation function

---
+++ scraper/utils.py
@@ -0,0 +1,17 @@
+
+def obtain_crypto_abbreviation(crypto_name: str) -> str:
+    """
+    Obtain the abbreviation of the cryptocurrencie
+    returns: str
+    """
+    abbreviation = ''
+    if crypto_name.isupper():
+        abbreviation = crypto_name[:len(crypto_name) // 2]
+    else:
+        for word in reversed(crypto_name):
+            if word.isupper():
+                abbreviation = word + abbreviation
+            else:
+                break
+
+    return abbreviation
